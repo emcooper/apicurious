b7795971084bdd2ed36825e955f59047ed3efc5e
Ellen Cooper <ellencooper74@gmail.com>
2017-08-22 16:17:42 -0600

    adds recent activity

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,12 @@
 class UsersController < ApplicationController
 
   def show
-    response = Faraday.get("https://api.github.com/users/#{current_user.username}")
+    response = Faraday.get("https://api.github.com/users/#{current_user.username}?client_id=#{ENV["github-key"]}&client_secret=#{ENV["github-secret"]}")
     attributes = JSON.parse(response.body, symbolize_names: :true)
-    starred_repos = Faraday.get("https://api.github.com/users/#{current_user.username}/starred")
-    num_starred_repos = JSON.parse(starred_repos.body).count
-    attributes[:num_starred_repos] = num_starred_repos
+    starred_repos = Faraday.get("https://api.github.com/users/#{current_user.username}/starred?client_id=#{ENV["github-key"]}&client_secret=#{ENV["github-secret"]}")
+    attributes[:num_starred_repos] = JSON.parse(starred_repos.body).count
+    recent_commits = Faraday.get("https://api.github.com/users/#{current_user.username}/events?client_id=#{ENV["github-key"]}&client_secret=#{ENV["github-secret"]}")
+    attributes[:recent_activity] = JSON.parse(recent_commits.body)
     @user = User.new(attributes)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,15 @@ class User < ApplicationRecord
   attr_reader :num_followers,
               :num_following,
               :profile_picture,
-              :num_starred_repos
+              :num_starred_repos,
+              :recent_activity
 
   def initialize(attributes)
     @num_followers = attributes[:followers]
     @num_following = attributes[:following]
     @num_starred_repos = attributes[:num_starred_repos]
     @profile_picture = attributes[:avatar_url]
+    @recent_activity = attributes[:recent_activity]
   end
 
   def self.from_omniauth(auth_info)
