82637eb2bc448d9687521b05d6783e6317809c08
Ellen Cooper <ellencooper74@gmail.com>
2017-08-22 14:10:53 -0600

    adds api call in user show

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+
+  def show
+    @user = current_user
+    # @conn = Faraday.new(url: "https://api.github.com") do |faraday|
+    #   faraday.headers["X-API-KEY"] = ENV["propublica-key"]
+    #   faraday.adapter Faraday.default_adapter
+    # end
+    response = Faraday.get("https://api.github.com/users/#{@user.username}") if @user
+    attributes = JSON.parse(response.body, symbolize_names: :true)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get '/auth/github', as: 'github_login'
   get '/auth/github/callback', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  get '/profile', to: 'users#show'
 end
