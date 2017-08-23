a1e09520304cb3b2eddc25f46df6141865a156c9
Ellen Cooper <ellencooper74@gmail.com>
2017-08-23 11:50:53 -0600

    finishes login test

--- spec/features/user_logs_in_spec.rb
+++ spec/features/user_logs_in_spec.rb
@@ -1,13 +1,15 @@
 RSpec.feature "user logs in" do
   scenario "using github oauth" do
-    stub_omniauth
-    visit root_path
+    VCR.use_cassette("features/user_logs_in") do
+      stub_omniauth
+      visit root_path
 
-    expect(page).to have_link("Login with Github")
+      expect(page).to have_link("Login with Github")
 
-    click_link "Login with Github"
+      click_link "Login with Github"
 
-    expect(page).to have_content("Ellen Cooper")
-    expect(page).to have_link("Logout")
+      expect(page).to have_content("Ellen Cooper")
+      expect(page).to have_link("Log Out")
+    end
   end
 end
