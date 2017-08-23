ae0a37ef765bcab09d2d8f426726bae7df59bf26
Ellen Cooper <ellencooper74@gmail.com>
2017-08-23 11:56:49 -0600

    adds stubbing and uncomments log out feature test; test now passes

--- spec/features/user_can_logout_spec.rb
+++ spec/features/user_can_logout_spec.rb
@@ -1,10 +1,14 @@
 RSpec.feature "User clicks log out" do
-  xit "they are logged out and redirected to root" do
+  it "they are logged out and redirected to root" do
+    VCR.use_cassette("features/user_can_logout") do
+      stub_omniauth
+      visit root_path
+      click_link "Login with Github"
 
-    visit '/'
-    click_on "Log Out"
-    expect(page).to_not have_content(user.name)
-    expect(page).to have_content("Login with Github")
-    expect(current_path).to eq(root)
+      click_on "Log Out"
+      expect(page).to_not have_content("Ellen Cooper")
+      expect(page).to have_link("Login with Github")
+      expect(current_path).to eq(root_path)
+    end
   end
 end
