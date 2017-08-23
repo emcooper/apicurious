4ceb44753c7545e34d47185fce73560e774120c8
Ellen Cooper <ellencooper74@gmail.com>
2017-08-23 14:01:25 -0600

    created github service; modified user_controller and dashboard to use service

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,6 @@ class UsersController < ApplicationController
     attributes[:num_starred_repos] = JSON.parse(starred_repos.body).count
     recent_commits = Faraday.get("https://api.github.com/users/#{current_user.username}/events?client_id=#{ENV["github-key"]}&client_secret=#{ENV["github-secret"]}")
     attributes[:recent_activity] = JSON.parse(recent_commits.body)
-    @user_dashboard = UserDashboard.new(attributes)
+    @user_dashboard = UserDashboard.new
   end
 end
--- app/models/user_dashboard.rb
+++ app/models/user_dashboard.rb
@@ -5,11 +5,11 @@ class UserDashboard
               :num_starred_repos,
               :recent_activity
 
-  def initialize(attributes)
-    @num_followers = attributes[:followers]
-    @num_following = attributes[:following]
-    @num_starred_repos = attributes[:num_starred_repos]
-    @profile_picture = attributes[:avatar_url]
-    @recent_activity = attributes[:recent_activity]
+  def initialize
+    @num_followers = GithubService.num_followers
+    @num_following = GithubService.num_following
+    @num_starred_repos = GithubService.num_starred_repos
+    @profile_picture = GithubService.profile_picture
+    @recent_activity = GithubService.recent_activity
   end
 end
